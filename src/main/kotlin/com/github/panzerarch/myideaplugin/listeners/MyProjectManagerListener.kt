cb005bdacd7f8bda7d71954ae8e889a0b0e7d1a0
GitHub Action <action@github.com>
2021-09-22 07:19:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/panzerarch/myideaplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/panzerarch/myideaplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.panzerarch.myideaplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.panzerarch.myideaplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/panzerarch/myideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/panzerarch/myideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.panzerarch.myideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.panzerarch.myideaplugin.MyBundle
 
 class MyApplicationService {
 
